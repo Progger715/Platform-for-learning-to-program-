2e2a0533393d017eb113b454c36e3ba985051ed0
Progger715 <bagdik5775@gmail.com>
2023-06-02 11:13:17 +0300

    added work with cookies with the theme 'loop tasks'

--- handler_code/tasks_generators/generator_loop_tasks.py
+++ handler_code/tasks_generators/generator_loop_tasks.py
@@ -1,18 +1,15 @@
 import random
 import subprocess
 from handler_code.collector_code import build_code_from_template
-
-while_limit: int
+from handler_code.collector_code import get_html_result_compiling
 
 
 def generate_loop_while_task():
     n = random.randint(5, 100)
-    global while_limit
-    while_limit = n
-    exercise = f"Вам дано число n = {n}. Используя цикл while, вычислите сумму всех чисел от 1 до n. " \
+    exercise = f"Вам дано число n = {n}.  Используя цикл while, вычислите сумму всех чисел от 1 до n. " \
                f"И выведите значение этой переменной с помощью команды System.out.println()"
     initial_code = f"int n = {n};\nint sum = 0;\n"
-    return exercise, initial_code
+    return exercise, initial_code, n
 
 
 # int sum = 0;
@@ -22,14 +19,12 @@ def generate_loop_while_task():
 #    i++;
 # }
 # System.out.println(sum);
-def check_correct_code_while(received_code: str):
-    path_to_file = build_code_from_template(received_code)
-    result = subprocess.run(['java', path_to_file], capture_output=True, text=True)
-    if result.stderr != "":
-        return "Ошибка компиляции! Проверьте свой код."
-    print(result)
+def check_correct_code_while(received_code: str, while_limit: int):
+    message, flag_errors, out_compiling = get_html_result_compiling(received_code)
+    if flag_errors:
+        return message
     answer = sum(i for i in range(1, while_limit + 1))
-    if str(answer) + "\n" == result.stdout:
+    if str(answer) + "\n" == out_compiling:
         return ("Поздравляем, Вы успешно справились с заданием!")
     else:
         return ("К сожалению, Вы не справились с заданием! Проверьте свой код.")
--- handler_code/views/api_view_loop_tasks.py
+++ handler_code/views/api_view_loop_tasks.py
@@ -2,16 +2,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 import handler_code.tasks_generators.generator_loop_tasks as task_generation
+from handler_code import helper_encryption
 
 
 class ApiViewLoopTask(APIView):
 
     def get(self, request):
-        task, initial_code = task_generation.generate_loop_while_task()
-        return Response({'task': {task}, 'inner_data': {initial_code}})
+        task, initial_code, data_for_answer = task_generation.generate_loop_while_task()
+        data_for_answer = helper_encryption.encrypt_data(str(data_for_answer))
+        return Response({'task': {task}, 'inner_data': {initial_code}, 'answer': {data_for_answer}})
 
     def post(self, request):
         received_code = request.data['code']
-        message = task_generation.check_correct_code_while(received_code)
+        data_for_answer = request.data['answer']
+        print("data_for_answer = ", data_for_answer)
+        print("type(data_for_answer) = ", type(data_for_answer))
+        data_for_answer = int(helper_encryption.decrypt_data(data_for_answer))
+        print("\nafter decryption")
+        print("data_for_answer = ", data_for_answer)
+        print("type(data_for_answer) = ", type(data_for_answer))
+        message = task_generation.check_correct_code_while(received_code, data_for_answer)
 
         return Response({'result': message})
