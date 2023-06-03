c90780039b1b0fa656bcf9a4b9e69428fc9c9986
Progger715 <bagdik5775@gmail.com>
2023-06-03 15:44:40 +0300

    created migrations for the database

--- users/models.py
+++ users/models.py
@@ -5,6 +5,6 @@ class UsersLogin(models.Model):
     login = models.CharField(max_length=35, unique=True)
     password = models.BinaryField()
     salt = models.BinaryField()
-    role = models.CharField()
+    role = models.CharField(max_length=35)
     date_registration = models.DateTimeField()
 
